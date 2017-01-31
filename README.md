4529c67d79df912799f1719a97932c5bceaf18d2
teanocrata <teanocrata@gmail.com>
2017-01-31 12:16:45 +0100

    55 Creación de test para RethinkDB y almacenamiento de ruta de imágenes

---
+++ README.md
@@ -0,0 +1,7 @@
+# Stack
+
+## npm
+## [standard](https://www.npmjs.com/package/standard)
+Asegura que el código sigue unas reglas de estilo
+## [AVA](https://github.com/avajs/ava)
+Librería de testing, bastante nueva que nos permite usar las nuevas funcionalidades de ecmascript
---
+++ test/utils-test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const test = require('ava')
+const utils = require('../lib/utils')
+
+test('extracting hashtags from text', t => {
+  let tags = utils.extractTags('a #pictures with tags #AwEsOmE #Platzi #AVA and #100 ##yes')
+
+  t.deepEqual(tags, ['pictures', 'awesome', 'platzi', 'ava', '100', 'yes'])
+
+  tags = utils.extractTags('a picture with no tags')
+  t.deepEqual(tags, [])
+
+  tags = utils.extractTags()
+  t.deepEqual(tags, [])
+
+  tags = utils.extractTags(null)
+  t.deepEqual(tags, [])
+})
